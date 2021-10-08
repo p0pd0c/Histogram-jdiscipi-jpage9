7170500e6a78479a83efc62bf8ba6a83eb198f23
Jared Michael Discipio <jdiscipi.silk23@uvm.edu>
2021-10-08 16:17:06 -0400

    serial done

--- histogram.jdiscipi.c
+++ histogram.jdiscipi.c
@@ -3,14 +3,35 @@
 #include <pthread.h>
 #include <stdlib.h>
 
+// Initialize the array
+int values[N];
+
+int tallySerial(int* histogram) {
+	int i;
+	for(i = 0; i < N; i++) {
+		histogram[values[i]] += 1;
+	}
+	return 0;	
+}
+
 int main() {
-	// Initialize the array
-	int values[N];
 	int i;
 	for(i = 0; i < N; i++) {
 		double r = drand48();
 		int val = (int) MAXVAL * r;
 		values[i] = val;
+	}
+
+	// Initialize Histogram
+	int histogram[MAXVAL];
+	for(i = 0; i < MAXVAL; i++) {
+		histogram[i] = 0;
+	}
+	// Run the serial histogram
+	int result = tallySerial(histogram);
+	// Look at buckets
+	for(i = 0; i < MAXVAL; i++) {
+		printf("Bucket: %d\n", histogram[i]);
 	}
 	return 0;
 }
