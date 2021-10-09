09688db5a93dcf5ee39adcb1a522b1bcb07a8301
Jared Michael Discipio <jdiscipi.silk23@uvm.edu>
2021-10-09 11:24:00 -0400

    tried to loop but threads will not join

--- histogram.jdiscipi.c
+++ histogram.jdiscipi.c
@@ -24,92 +24,100 @@ int main() {
 	// Initialize time vals
 	struct timeval t1, t2;
 	float elapsedTime;
-	// Time Serial
-	// Initialize Serial Histogram
+	float times[3][5];
+	int j;
 	int serialHistogram[MAXVAL];
-	for(i = 0; i < MAXVAL; i++) {
-		serialHistogram[i] = 0;
+	for(j = 0; j < 5; j++) {	
+		// Time Serial
+		// Initialize Serial Histogram
+		for(i = 0; i < MAXVAL; i++) {
+			serialHistogram[i] = 0;
+		}
+		gettimeofday(&t1, NULL);
+		tallySerial(serialHistogram);
+		gettimeofday(&t2, NULL);
+		elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000.0; // sec to usec
+		elapsedTime += (t2.tv_usec - t1.tv_usec) / 1000.0; // usec to ms 
+		printf("Serial time: %f\n", elapsedTime);
+		times[0][j] = elapsedTime;
 	}
-	gettimeofday(&t1, NULL);
-	tallySerial(serialHistogram);
-	gettimeofday(&t2, NULL);
-	elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000.0; // sec to usec
-	elapsedTime += (t2.tv_usec - t1.tv_usec) / 1000.0; // usec to ms 
-	printf("Serial time: %f\n", elapsedTime);
-
-
-	// Time Bad
-	// Initialize Bad Histogram
 	int badHistogram[MAXVAL];
-	for(i = 0; i < MAXVAL; i++) {
-		badHistogram[i] = 0;
+	for(j = 0; j < 5; j++) {
+		// Time Bad
+		// Initialize Bad Histogram
+		int badHistogram[MAXVAL];
+		for(i = 0; i < MAXVAL; i++) {
+			badHistogram[i] = 0;
+		}
+		// Initialize Bad Data
+		// Create data for threads
+		pthread_mutex_t badMutex;
+		ThreadInfo* badThreadData[NUM_THREADS];
+		for(i = 1; i <= NUM_THREADS; i++) {
+			ThreadInfo* threadDatum = malloc(sizeof(ThreadInfo));
+			threadDatum->startIndex = (N * (i - 1)) / NUM_THREADS;
+			threadDatum->endIndex = (N * i) / NUM_THREADS - 1; 
+			threadDatum->histogram = badHistogram;
+			threadDatum->mutex = &badMutex;
+			badThreadData[i - 1] = threadDatum;
+		}
+		pthread_t tIdsBad[NUM_THREADS];
+		// Start timer
+		gettimeofday(&t1, NULL);
+		// Start threads
+		for(i = 0; i < NUM_THREADS; i++) {
+			pthread_t tId;
+			tIdsBad[i] = tId;
+			pthread_create(&tIdsBad[i], NULL, tallyBad, badThreadData[i]);
+		}
+		// Wait for all threads
+		for(i = 0; i < NUM_THREADS; i++) {
+			pthread_join(tIdsBad[i], NULL);
+		}
+		gettimeofday(&t2, NULL);
+		elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000.0; // sec to usec
+		elapsedTime += (t2.tv_usec - t1.tv_usec) / 1000.0; // usec to ms 
+		printf("Bad time: %f\n", elapsedTime);
+		times[1][j] = elapsedTime;
 	}
-	// Initialize Bad Data
-	// Create data for threads
-	pthread_mutex_t badMutex;
-	ThreadInfo* badThreadData[NUM_THREADS];
-	for(i = 1; i <= NUM_THREADS; i++) {
-		ThreadInfo* threadDatum = malloc(sizeof(ThreadInfo));
-		threadDatum->startIndex = (N * (i - 1)) / NUM_THREADS;
-		threadDatum->endIndex = (N * i) / NUM_THREADS - 1; 
-		threadDatum->histogram = badHistogram;
-		threadDatum->mutex = &badMutex;
-		badThreadData[i - 1] = threadDatum;
+	int goodHistogram[MAXVAL];
+	for(j = 0; j < 5; j++) {
+		// Time Good
+  	// Initialize Good Histogram
+  	for(i = 0; i < MAXVAL; i++) {
+    	goodHistogram[i] = 0;
+  	}
+  	// Initialize Good Data
+  	// Create data for threads
+  	pthread_mutex_t goodMutex;
+  	ThreadInfo* goodThreadData[NUM_THREADS];
+  	for(i = 1; i <= NUM_THREADS; i++) {
+    	ThreadInfo* threadDatum = malloc(sizeof(ThreadInfo));
+    	threadDatum->startIndex = (N * (i - 1)) / NUM_THREADS;
+    	threadDatum->endIndex = (N * i) / NUM_THREADS - 1;
+    	threadDatum->histogram = goodHistogram;
+    	threadDatum->mutex = &goodMutex;
+    	goodThreadData[i - 1] = threadDatum;
+  	}
+  	pthread_t tIdsGood[NUM_THREADS];
+  	// Start timer
+  	gettimeofday(&t1, NULL);
+  	// Start threads
+  	for(i = 0; i < NUM_THREADS; i++) {
+    	pthread_t tId;
+    	tIdsGood[i] = tId;
+    	pthread_create(&tIdsGood[i], NULL, tallyGood, goodThreadData[i]);
+  	}
+  	// Wait for all threads
+  	for(i = 0; i < NUM_THREADS; i++) {
+    	pthread_join(tIdsGood[i], NULL);
+  	}
+  	gettimeofday(&t2, NULL);
+  	elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000.0; // sec to usec
+  	elapsedTime += (t2.tv_usec - t1.tv_usec) / 1000.0; // usec to ms 
+  	printf("Good time: %f\n", elapsedTime);
+		times[2][j] = elapsedTime;
 	}
-	pthread_t tIds[NUM_THREADS];
-	// Start timer
-	gettimeofday(&t1, NULL);
-	// Start threads
-	for(i = 0; i < NUM_THREADS; i++) {
-		pthread_t tId;
-		tIds[i] = tId;
-		pthread_create(&tIds[i], NULL, tallyBad, badThreadData[i]);
-	}
-	// Wait for all threads
-	for(i = 0; i < NUM_THREADS; i++) {
-		pthread_join(tIds[i], NULL);
-	}
-	gettimeofday(&t2, NULL);
-	elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000.0; // sec to usec
-	elapsedTime += (t2.tv_usec - t1.tv_usec) / 1000.0; // usec to ms 
-	printf("Bad time: %f\n", elapsedTime);
-	
-	// Time Good
-  // Initialize Good Histogram
-  int goodHistogram[MAXVAL];
-  for(i = 0; i < MAXVAL; i++) {
-    goodHistogram[i] = 0;
-  }
-  // Initialize Good Data
-  // Create data for threads
-  pthread_mutex_t goodMutex;
-  ThreadInfo* goodThreadData[NUM_THREADS];
-  for(i = 1; i <= NUM_THREADS; i++) {
-    ThreadInfo* threadDatum = malloc(sizeof(ThreadInfo));
-    threadDatum->startIndex = (N * (i - 1)) / NUM_THREADS;
-    threadDatum->endIndex = (N * i) / NUM_THREADS - 1;
-    threadDatum->histogram = goodHistogram;
-    threadDatum->mutex = &goodMutex;
-    goodThreadData[i - 1] = threadDatum;
-  }
-  pthread_t tIdsGood[NUM_THREADS];
-  // Start timer
-  gettimeofday(&t1, NULL);
-  // Start threads
-  for(i = 0; i < NUM_THREADS; i++) {
-    pthread_t tId;
-    tIdsGood[i] = tId;
-    pthread_create(&tIdsGood[i], NULL, tallyGood, goodThreadData[i]);
-  }
-  // Wait for all threads
-  for(i = 0; i < NUM_THREADS; i++) {
-    pthread_join(tIds[i], NULL);
-  }
-  gettimeofday(&t2, NULL);
-  elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000.0; // sec to usec
-  elapsedTime += (t2.tv_usec - t1.tv_usec) / 1000.0; // usec to ms 
-  printf("Good time: %f\n", elapsedTime);
-	
 	// Test equality of all histograms
 	int dataIsGood = 1;
 	for(i = 0; i < MAXVAL; i++) {
@@ -122,6 +130,20 @@ int main() {
 	} else {
 		printf("Histograms are not equal\n");
 	}
+	
+	// Calculate Average Run Time
+	float averages[3];
+	for(i = 0; i < 3; i++) {
+		int j;
+		averages[i] = 0;
+		for(j = 0; j < 5; j++) {
+			averages[i] += times[i][j]; 
+		}
+		averages[i] /= 5.0;
+	}
+	printf("Serial Average: %f\n", averages[0]);
+	printf("Bad Average: %f\n", averages[1]);
+	printf("Good Average %f\n", averages[2]);
 
 	return 0;
 }
